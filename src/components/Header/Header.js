7b3b935ddaaba72170023915dca13a30176c3e46
thedu <dunalkonrad@gmail.com>
2022-11-29 18:17:10 +0100

    Add header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,8 +1,26 @@
 import React from 'react';
+import './Header.css';
+import Logo from "../../olx-logo.svg";
+import {FaMapMarkerAlt, FaSearch, FaUserCircle} from "react-icons/fa";
 
 function Header() {
     return (
-        <h1>HEADER</h1>
+        <div className={"Header"}>
+            <div className={"HeaderLeft"}>
+                <img src={Logo} className={"Logo"}/>
+            </div>
+            <div className={"HeaderCenter"}>
+                <FaSearch className={"Icon"}/>
+                <input type={"text"} placeholder={"Szukaj"} className={"CenterSearch"}/>
+                <FaMapMarkerAlt className={"Icon"}/>
+                <input type={"text"} placeholder={"Lokalizacja"} className={"CenterLocalization"}/>
+            </div>
+            <div className={"HeaderRight"}>
+                <button className={"HeaderRightSell"}>DODAJ OGŁOSZENIE</button>
+                <h4 className={"HeaderRightLogin"}>Logowanie</h4>
+                <FaUserCircle className={"User"}/>
+            </div>
+        </div>
     );
 }
 
